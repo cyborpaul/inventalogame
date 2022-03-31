70ec35b800600f57ac58e59ff8dcc28fc23e03b4
cyborpaul <leowaldir2000@gmail.com>
2022-03-31 12:57:16 -0500

    EDITAR ASIGNATURAS44

---
+++ prueba.php
@@ -0,0 +1,7 @@
+<?php
+$id=$_GET['id'];
+$servicio=$_GET['servicio'];
+require("config.php");
+$sql= "UPDATE `inventalogame_servicios` SET `ser_varchar_servicio`=$servicio WHERE ser_int_id='$id'";
+$ejecutado=mysqli_query($mysqli,$sql);
+?>
